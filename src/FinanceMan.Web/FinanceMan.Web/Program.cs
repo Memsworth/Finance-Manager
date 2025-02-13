c3d6ee4143760cf8edc13ee9bd030756f563ec25
Abubakar Ahmed <abubakarmahfooz4479@outlook.com>
2025-02-13 16:55:17 +0300

    clean write. Added empty mud blazor wasm global

--- src/FinanceMan.Web/FinanceMan.Web/Program.cs
+++ src/FinanceMan.Web/FinanceMan.Web/Program.cs
@@ -1,35 +1,11 @@
 using MudBlazor.Services;
 using FinanceMan.Web.Components;
-using FinanceMan.Database;
-using FinanceMan.Domain.Contracts;
-using FinanceMan.Database.Repositories;
-using FinanceMan.Shared.Contracts;
-using FinanceMan.Web.Services;
-using AspNetCore.Swagger.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
-builder.Services.AddDbContext<ApplicationDbContext>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddControllers();
-builder.Services.AddOpenApi(options =>
-{
-    options.AddDocumentTransformer((document, _, _) =>
-    {
-        document.Info = new()
-        {
-            Title = "Finance Manager API",
-            Version = "v1",
-            Description = "API for processing finance data for the FinanceMan Application."
-        };
-        return Task.CompletedTask;
-    });
-});
 
-builder.Services.AddEndpointsApiExplorer();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
@@ -39,11 +15,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwaggerUI(ModernStyle.Dark, options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI V1");
-    });
     app.UseWebAssemblyDebugging();
 }
 else
@@ -59,7 +30,6 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapControllers();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(FinanceMan.Web.Client._Imports).Assembly);
